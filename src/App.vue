64cef0d04a0c310b587643d1ec4b492f09f7a5b6
Bryan <Bryan.com>
2025-02-26 18:46:04 +0800

    加入提示字符

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Controls @render-json="renderJson" @search="performSearch" @collapse-all="collapseAll" @toggle-dark-mode="toggleDarkMode" />
     <JsonViewer :json-data="jsonData" ref="jsonViewerRef" />
     <button id="back-to-top" class="fixed-button" @click="scrollToTop">回到顶部</button>
+    <KeyPressDisplay />
   </div>
 </template>
 
@@ -10,6 +11,7 @@
 import { ref } from 'vue';
 import Controls from './components/Controls.vue';
 import JsonViewer from './components/JsonViewer.vue';
+import KeyPressDisplay from './components/KeyPressDisplay.vue';
 import { defaultJson } from './utils/JsonUtils';
 
 const jsonData = ref<any>(defaultJson); // Initial JSON data
---
+++ src/components/KeyPressDisplay.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <div
+      v-for="item in keys"
+      :key="item.id"
+      :style="{ bottom: getBottomPosition(keys.indexOf(item)) + 'px' }"
+      class="key-display"
+      @animationend="removeKey(item.id)"
+    >
+      {{ item.key }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      keys: [], // 存储对象 { id, key }
+      idCounter: 0 // 用于生成唯一ID
+    };
+  },
+  mounted() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  },
+  beforeUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  },
+  methods: {
+    handleKeyDown(event) {
+      // const key = event.key.slice(0,3);
+      const key = event.key;
+      // 添加带有唯一ID的对象
+      this.keys.unshift({
+        id: this.idCounter++,
+        key: key
+      });
+    },
+    getBottomPosition(index) {
+      return 80 + index * 60;
+    },
+    removeKey(id) {
+      // 查找并移除特定ID的元素
+      const index = this.keys.findIndex(item => item.id === id);
+      if (index !== -1) {
+        this.keys.splice(index, 1);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+@keyframes stayVisible {
+  from {
+    opacity: 1;
+    transform: translateY(0);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+@keyframes fadeOut {
+  from {
+    opacity: 1;
+    transform: translateY(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateY(-1px);
+  }
+}
+
+.key-display {
+  position: fixed;
+  right: 20px;
+  background-color: rgba(0, 0, 0, 0.7);
+  color: white;
+  padding: 10px;
+  border-radius: 5px;
+  font-size: 24px;
+  min-width: 60px;
+  height: 50px;
+  text-align: center;
+  line-height: 25px;
+  border: 1px solid rgba(255, 255, 255, 0.3);
+  box-sizing: border-box;
+  margin-bottom: 10px;
+  opacity: 1;
+  /* 先执行 200ms 的 stayVisible 动画，然后执行 1s 的 fadeOut 动画 */
+  animation: stayVisible 1s ease, fadeOut 1s ease-out 200ms forwards;
+}
+</style>
